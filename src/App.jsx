cfb751c05d07975747b2fdc6ce016ba56131579a
mathiasthomas17 <thomasmathias17@gmail.com>
2025-01-22 05:23:31 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import Favorites from './pages/Favorites'
 
 function App() {
 
   return (
-    <div>
-      <Home />
-    </div>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+    </main>
   )
 }
 
